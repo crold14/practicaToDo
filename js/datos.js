815189d3dfa833b9425a97fe0a636b3ad0fd41f7
crold14 <carlos_roldan92@hotmail.com>
2022-03-07 17:53:11 +0100

    entrega final

---
+++ js/datos.js
@@ -0,0 +1,19 @@
+let arrTareas = new Array();
+arrTareas = [
+    {
+        'idTarea': 0,
+        'titulo': 'Estudiar Javascript',
+        'prioridad': 'urgente'
+    },
+    {
+        'idTarea': 2,
+        'titulo': 'Dormir',
+        'prioridad': 'diaria'
+    },
+    {
+        'idTarea': 3,
+        'titulo': 'Salir a comer',
+        'prioridad': 'mensual'
+    },
+
+];
